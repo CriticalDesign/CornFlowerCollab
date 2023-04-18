2c439ddfc4d1ab8c98beefcc4c250e5e30ec238e
CriticalDesign <aaron.langille@cambriancollege.ca>
2023-04-18 16:07:25 -0400

    Changed windows to Green!!!

--- Game1.cs
+++ Game1.cs
@@ -42,7 +42,7 @@ namespace CornFlowerCollab
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Green);
 
             // TODO: Add your drawing code here
 
